f290327049a180d7f9b6d55f1695419a3bddaf3f
VV <vasileva.vesela@gmail.com>
2022-07-27 17:28:38 +0200

    Suite exercices

---
+++ back/ecosystem.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  apps: [
+    {
+      name: "app1",
+      node_args: "-r ts-node/register -r tsconfig-paths/register",
+      script: "./dist/src/index.js",
+      instances: "2",
+      exec_mode: "cluster",
+      env: {
+        PORT: 3001,
+        TS_NODE_BASEURL: "./dist",
+      },
+    },
+  ],
+};
