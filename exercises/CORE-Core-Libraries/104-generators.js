f83bd0e52c270975ff6e36d101c091ee1a305148
Gabriel Pichot <gabriel.pichot@gmail.com>
2023-02-06 13:46:20 +0100

    feat: add generator exercise

---
+++ exercises/CORE-Core-Libraries/104-generators.js
@@ -0,0 +1,8 @@
+console.log("--- Exercise 1 ---");
+
+// 1. Write a generator that yields the cube of numbers
+//
+// 2. Print the first 3 cubes using the generator function
+//
+// 3. Print the 10 cubes using `for (const ... of ...)`
+//
